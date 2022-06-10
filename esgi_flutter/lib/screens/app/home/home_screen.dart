eb7d819237b9e24a03ab050052a110ae1b999370
BenoitPE <60927544+BenoitPE@users.noreply.github.com>
2022-06-10 14:51:07 +0200

    Ajout d'une SingleChildScrollView et correction de la ListView

--- esgi_flutter/lib/screens/app/home/home_screen.dart
+++ esgi_flutter/lib/screens/app/home/home_screen.dart
@@ -29,45 +29,35 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.topLeft,
-        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            const Text(
-              'MES NOTES',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.left,
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-              decoration:
-                  BoxDecoration(border: Border.all(color: Colors.black)),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  const Text(
-                    'NOUVELLE NOTE',
-                    textAlign: TextAlign.left,
-                  ),
-                  TextFormField(
-                    minLines: 1,
-                    maxLines: 1,
-                    decoration: const InputDecoration(
-                      labelText: 'Titre',
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black, width: 0.0),
-                      ),
-                      border: OutlineInputBorder(),
+      body: SingleChildScrollView(
+        child: Container(
+          alignment: Alignment.topLeft,
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              const Text(
+                'MES NOTES',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.left,
+              ),
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.black)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    const Text(
+                      'NOUVELLE NOTE',
+                      textAlign: TextAlign.left,
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 25),
-                    child: TextFormField(
-                      maxLines: 5,
+                    TextFormField(
+                      minLines: 1,
+                      maxLines: 1,
                       decoration: const InputDecoration(
-                        labelText: 'Contenu',
+                        labelText: 'Titre',
                         enabledBorder: OutlineInputBorder(
                           borderSide:
                               BorderSide(color: Colors.black, width: 0.0),
@@ -75,63 +65,75 @@ class _HomePageState extends State<HomePage> {
                         border: OutlineInputBorder(),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 25.0),
-                    child: TextButton(
-                      style: ButtonStyle(
-                        overlayColor:
-                            MaterialStateProperty.all<Color>(Colors.grey),
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.black),
-                        foregroundColor:
-                            MaterialStateProperty.all<Color>(Colors.white),
-                      ),
-                      onPressed: () {},
-                      child: const Text('Ajouter une image'),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 25.0),
-                    child: TextButton(
-                      style: ButtonStyle(
-                        overlayColor:
-                            MaterialStateProperty.all<Color>(Colors.grey),
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.black),
-                        foregroundColor:
-                            MaterialStateProperty.all<Color>(Colors.white),
-                      ),
-                      onPressed: () {},
-                      child: const Text(
-                        'AJOUTER MA NOTE',
-                        style: TextStyle(fontWeight: FontWeight.bold),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 25),
+                      child: TextFormField(
+                        maxLines: 5,
+                        decoration: const InputDecoration(
+                          labelText: 'Contenu',
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.black, width: 0.0),
+                          ),
+                          border: OutlineInputBorder(),
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(top: 25),
-              child: Text(
-                'MES NOTES SAUVEGARDÉES',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+                    Padding(
+                      padding: const EdgeInsets.only(top: 25.0),
+                      child: TextButton(
+                        style: ButtonStyle(
+                          overlayColor:
+                              MaterialStateProperty.all<Color>(Colors.grey),
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.black),
+                          foregroundColor:
+                              MaterialStateProperty.all<Color>(Colors.white),
+                        ),
+                        onPressed: () {},
+                        child: const Text('Ajouter une image'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 25.0),
+                      child: TextButton(
+                        style: ButtonStyle(
+                          overlayColor:
+                              MaterialStateProperty.all<Color>(Colors.grey),
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.black),
+                          foregroundColor:
+                              MaterialStateProperty.all<Color>(Colors.white),
+                        ),
+                        onPressed: () {},
+                        child: const Text(
+                          'AJOUTER MA NOTE',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                textAlign: TextAlign.left,
               ),
-            ),
-            Container(
-                height: 300,
-                child: ListView.separated(
-                  itemCount: notes.length,
-                  separatorBuilder: (context, _) => SizedBox(width: 12),
-                  itemBuilder: (context, index) =>
-                      buildNote(note: notes[index], context: context),
-                )),
-          ],
+              const Padding(
+                padding: EdgeInsets.only(top: 25),
+                child: Text(
+                  'MES NOTES SAUVEGARDÉES',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: notes.length,
+                itemBuilder: (context, index) =>
+                    buildNote(note: notes[index], context: context),
+              ),
+            ],
+          ),
         ),
       ),
     );
